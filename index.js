8dd300cc305f4a9a4b32414a7b9189db9ea5fc88
New Author Name <vanaigranov@gmail.com>
2023-09-26 23:24:07 -0500

    add robot

--- index.js
+++ index.js
@@ -14,6 +14,72 @@ const roomDoor = 2;
 const rem = parseFloat(getComputedStyle(document.documentElement).fontSize)// doesn't recalculate when font size changes :/
 const squareSize = 3 * rem;
 
+{
+    const roomW = document.getElementById('room-w');
+    const roomH = document.getElementById('room-h');
+    size.w = Number(roomW.value);
+    size.h = Number(roomH.value);
+
+    roomW.addEventListener('change', () => { size.w = Number(roomW.value); updateRoomCanvas() })
+    roomH.addEventListener('change', () => { size.h = Number(roomH.value); updateRoomCanvas() })
+}
+
+const robotHistory = [];
+var robotPosI = 0;
+var algorithm;
+
+{
+    const beg  = document.getElementById('robot-beg' );
+    const prev = document.getElementById('robot-prev');
+    const next = document.getElementById('robot-next');
+    const end  = document.getElementById('robot-end' );
+
+    beg.addEventListener('click', () => {
+        robotPosI = 0;
+        updateRoomCanvas();
+    });
+    prev.addEventListener('click', () => {
+        if(robotPosI === 0) return;
+        robotPosI--;
+        updateRoomCanvas();
+    });
+    next.addEventListener('click', () => {
+        moveNextState();
+        updateRoomCanvas();
+    });
+    end.addEventListener('click', () => {
+        robotPosI = robotHistory.length-1;
+        let i = 0;
+        while(i < 10000 && moveNextState()) i++;
+        if(i === 10000) console.error('too much iterations');
+        updateRoomCanvas();
+    });
+}
+
+
+updateRoomCanvas();
+
+function moveNextState() {
+    if(robotPosI+1 < robotHistory.length-1) {
+        robotPosI++;
+        return true;
+    }
+
+    const res = algorithm.next();
+    if(res.done) return false;
+
+    robotPosI = robotHistory.length;
+    robotHistory.push(res.value);
+    return true;
+}
+
+function* robotAlgorithm(x, y, cellAt) {
+    yield { x, y };
+    while(cellAt(x, y-1) === roomEmpty) {
+        y--
+        yield { x, y };
+    }
+}
 
 roomCanvas.addEventListener('mouseup', function(e) {
     const rect = roomCanvas.getBoundingClientRect();
@@ -43,18 +109,6 @@ function updateCell(x, y, cellType) {
         room[y*size.w+x] = cellType;
     }
     updateRoomCanvas();
-}
-
-{
-    const roomW = document.getElementById('room-w');
-    const roomH = document.getElementById('room-h');
-    size.w = Number(roomW.value);
-    size.h = Number(roomH.value);
-
-    updateRoomCanvas();
-
-    roomW.addEventListener('change', () => { size.w = Number(roomW.value); updateRoomCanvas() })
-    roomH.addEventListener('change', () => { size.h = Number(roomH.value); updateRoomCanvas() })
 }
 
 function updateRoomCanvas() {
@@ -89,6 +143,16 @@ function updateRoomCanvas() {
         }
     }
 
+    {
+        const pos = robotHistory[robotPosI];
+        const x = pos.x * squareSize, y = pos.y * squareSize;
+        const r = squareSize*0.5;
+
+        context.fillStyle = 'red';
+        context.beginPath();
+        context.arc(x+r, y+r, r, 0, 2 * Math.PI);
+        context.fill();
+    }
 }
 
 function clamp(num, a, b) { return Math.max(Math.min(num, Math.max(a, b)), Math.min(a, b)); }
@@ -121,6 +185,15 @@ function resizeBoard(newW, newH) {
 
     prevSize.w = newW;
     prevSize.h = newH;
+
+    resetRobot(newW, newH);
+}
+
+function resetRobot(w, h) {
+    robotHistory.length = 0;
+    algorithm = robotAlgorithm(Math.floor(w/2), Math.floor(h/2), (x, y) => room[y*w+x] ?? roomWall);
+    robotHistory.push(algorithm.next().value);
+    robotPosI = 0;
 }
 
 
